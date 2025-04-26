1aac173523aa4a1f0538db75f3494d2542a47ca3
Frankie Guapo <frankie.guapo@gmail.com>
2025-04-26 09:25:29 -0500

    Creating separate category pages

--- app.py
+++ app.py
@@ -39,6 +39,6 @@ def serve_testimonials():
     all_testimonials = Testimonial.query.all()
     return render_template("testimonials.html", all_testimonials=all_testimonials)
 
-#@app.route('/art-gallery')
-#def serve_gallery():
-#    return render_template("gallery.html")
+@app.route('/web-apps')
+def show_web_app_projects():
+    return render_template("web_apps.html")
